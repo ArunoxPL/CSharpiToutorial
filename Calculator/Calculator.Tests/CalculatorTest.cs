83d785a8b4d4f4b3cdd7439c78570d0f01354468
ArunoxPL <greg.malek@gmail.com>
2017-07-12 14:08:44 +0200

    NUnit first attempts

---
+++ Calculator/Calculator.Tests/CalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            var sut = new SimpleCalc();
+
+            var result = sut.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            var sut = new SimpleCalc();
+
+            var result = sut.Multiply(2, 10);
+
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+    }
+}
